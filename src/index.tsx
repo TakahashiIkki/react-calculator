c6beed734ec8a0e0744157f05d1c5bea648554e6
Takashi Ikki <new_ittyandesu@outlook.jp>
2022-06-03 23:57:54 +0900

    top

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './components/pages/Top';
+import { Top } from './components/pages/Top';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Top />
   </React.StrictMode>,
 );
